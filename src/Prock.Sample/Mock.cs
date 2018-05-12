f343f3f2074e565d9f71e77abeeb98fba79990e6
mcanete <mcanete@plainconcepts.com>
2018-05-12 06:53:19 +0200

    Add prock sample host

---
+++ src/Prock.Sample/Mock.cs
@@ -0,0 +1,24 @@
+﻿using Prock.Core;
+
+namespace Prock.Sample
+{
+    public class Mock : IMock
+    {
+        public Mock()
+        {
+            this.Json = "{}";
+            this.StatusCode = 200;
+            this.ContentType = "application/json";
+        }
+
+        public string Route { get; set; }
+
+        public string FilePath { get; set; }
+
+        public string Json { get; set; }
+
+        public int StatusCode { get; set; }
+
+        public string ContentType { get; set; }
+    }
+}
---
+++ src/Prock.Sample/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Prock.Core;
+using System.IO;
+using System.Linq;
+
+namespace Prock.Sample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            var mocks = Configuration.GetSection(nameof(Mock)).Get<Mock[]>();
+            foreach (var mock in mocks.Where(m => m.FilePath != null))
+            {
+                mock.Json = File.ReadAllText(mock.FilePath);
+            }
+
+            app.UseMocks(mocks);
+        }
+    }
+}
